73d450abca9c029f562a5d574905cdc9b06f1b6a
NicolaeTiberius <60753219+NicolaeTiberius@users.noreply.github.com>
2022-09-26 21:33:49 +0100

    App commited
    An one-page that uses the IMDB api to fetch any movie you search for, and displays all of the records of that movie title with the year it was produced. 
    Very simple React functionality making use of hooks better, useState  and useEffects, also using props better to transfer data dynamically through components.

---
+++ video-app-git/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from './App';
+
+//trigger the div and inject entire app application
+ReactDOM.render(<App/>,document.getElementById('root'));
